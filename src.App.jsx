8fc1e3d50723dfba81e76a185bcbc84f10ad8ab1
Marianne Hämmerle <marianne.haemmerle8@gmail.com>
2025-08-16 13:29:50 +0300

    Create src.App.jsx

---
+++ src.App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Willkommen in meiner App 🚀</h1>
+      <p>Diese Seite rendert jetzt korrekt Inhalte.</p>
+    </div>
+  );
+}
+
+export default App;
